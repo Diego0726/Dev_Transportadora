7ef57242c7ff9190dba835cfca56cce1ce79f178
Rodrigo Silva <rodrigomota.silva@hotmail.com>
2023-05-12 20:42:42 -0300

    Create Base de Dados.py

---
+++ Base de Dados/Base de Dados.py
@@ -0,0 +1,22 @@
+nomeCliente=''
+Email=''
+Contato=0
+Tonelada=0
+Kmrodado=0
+Valorgasolina=5.23
+Mcarros=['Truck','Bitruck','Cavalo Mecanico','Toco']
+calc=0
+
+nomeCliente=(input('Qual seu nome: '))
+Email=(input('Seu email para contato: '))
+Contato=(input('Seu telefone para contato: '))
+Tonelada=int(input('Quantoas toneladas serão transportadas: '))
+Kmrodado=int(str (input('Qual a distância rodada: ')))
+
+#truck= 22km/l; Bittruck= 17km/l; Cavalo Mecanico= 14km/l; Toco=8km/l;
+if Tonelada >= 5 :
+    calc= 
+    print("Você vai usar o Truck ")
+elif Tonelada < 5 :
+    print("Nao vai dar parceiro")
+
