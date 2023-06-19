3d7db6a58f5e87ad8cd46c454aaa34bc9656a1c6
Diego_Barreto <133256320+Diego0726@users.noreply.github.com>
2023-06-19 17:19:52 -0300

    Código Documentado
    Documentação do codigo

--- Front-end- HTML e CSS/Inicio.php
+++ Front-end- HTML e CSS/Inicio.php
@@ -1,34 +1,33 @@
 <?php
-include(`Conexão.php`);
+include(`Conexão.php`); // Inclui o arquivo "Conexão.php" para estabelecer a conexão com o banco de dados
 
 if(isset($_POST['email']) == 0 ){
-    echo"Preencha se e-mail";
+    echo"Preencha se e-mail"; // Verifica se o campo de email foi preenchido. Caso contrário, exibe a mensagem de erro.
 } else if(strlen($_POST['senha']) == 0){
-    echo "Preencha sua Senha";
+    echo "Preencha sua Senha"; // Verifica se o campo de senha foi preenchido. Caso contrário, exibe a mensagem de erro.
 } else {
 
-    $email = $mysqli->real_escape_string($_POST['email']);
-    $email = $mysqli->real_escape_string($_POST['senha']);
+    $email = $mysqli->real_escape_string($_POST['email']); // Obtém o valor do campo de email e protege contra ataques de SQL injection
+    $email = $mysqli->real_escape_string($_POST['senha']); // Obtém o valor do campo de senha e protege contra ataques de SQL injection
 
-    $sql_code = "Select * FROM usuarios WHERE email = '$email' AND senha = '$senha'";
-    $sql_query = $mysqli->query($sql_code) or die("Falha na execução do código SQL:" . $mysqli->error);
+    $sql_code = "Select * FROM usuarios WHERE email = '$email' AND senha = '$senha'"; // Cria uma consulta SQL para verificar se o email e senha correspondem a um registro na tabela "usuarios"
+    $sql_query = $mysqli->query($sql_code) or die("Falha na execução do código SQL:" . $mysqli->error); // Executa a consulta SQL no banco de dados e armazena o resultado na variável $sql_query
 
-    $quantidade = $sql_query->num_rows;
+    $quantidade = $sql_query->num_rows; // Obtém o número de linhas retornadas pela consulta
 
     if($quantidade == 1) {
-        $usuario = $sql_query->fetch_assoc();
+        $usuario = $sql_query->fetch_assoc(); // Obtém os dados do usuário encontrado no banco de dados
 
         if(!isset($_SESSION)) {
-            session_start();
+            session_start(); // Inicia a sessão do PHP, se ainda não estiver iniciada
         }
-        $_SESSION['user'] = $usuario['id'];
-        $_SESSION['nome'] = $usuario['nome'];
+        $_SESSION['user'] = $usuario['id']; // Armazena o ID do usuário na variável de sessão 'user'
+        $_SESSION['nome'] = $usuario['nome']; // Armazena o nome do usuário na variável de sessão 'nome'
 
-            header(("Location: Seleção.html"));    
+        header(("Location: Seleção.html")); // Redireciona o usuário para a página "Seleção.html" após o login bem-sucedido    
     } else {
-        echo "Falha ao logar! E-mail ou senha incorretos";
+        echo "Falha ao logar! E-mail ou senha incorretos"; // Exibe a mensagem de erro caso o email e senha não correspondam a nenhum registro na tabela "usuarios"
     }
-
 }
 ?>
 <!DOCTYPE html>
